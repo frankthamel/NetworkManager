71b1c6cfd510b2580f172cba01fe7e915f650048
Frank Thamel <w.frankthamel@gmail.com>
2024-11-05 23:40:47 +0000

    Add documentation

--- Package.swift
+++ Package.swift
@@ -7,14 +7,11 @@ let package = Package(
     name: "NetworkManager",
     platforms: [.iOS(.v13), .macOS(.v10_15), .watchOS(.v6)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NetworkManager",
             targets: ["NetworkManager"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "NetworkManager"),
         .testTarget(
